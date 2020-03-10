9f7accc649a5941e1da787b08d80db60e26e6b50
benfarrow <57004160+benfarrow@users.noreply.github.com>
2020-03-10 13:41:55 +0000

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,90 @@
+
+// const cafe = {
+//     name: "Whitesheep",
+//     seatingCapacity: 100,
+//     hasSpecialOffers: true,
+//     drinks: [
+//         "Cappuccino",
+//         "Latte",
+//         "Filter coffee",
+//         "Tea",
+//         "Hot chocolate"
+// ] };
+// // sets up cafe "whitesheep" const drinks list and info
+
+// const person = {
+//     name: "Ben",
+//     age: "32",
+// }
+// console.log (person["name"])
+// // pulls out info from constant
+
+// let offer = "none";
+// let time = 1200;
+// const cafe = {
+//     name: "Whitesheep",
+//     seatingCapacity: 100,
+//     hasSpecialOffers: true,
+//     drinks: [
+//         "Cappuccino",
+//         "Latte",
+//         "Filter coffee",
+//         "Tea",
+//         "Hot chocolate"
+// ],
+//     breakfastOffer: "Free croissant with coffee",
+//     lunchOffer: "Free drink with surprisingly priced sandwich",
+//     noOffer: "Sorry no offer"
+// };
+
+// // base-> before we return bottom statements depending on time of day from "let time= x"
+
+// let offer = "none";
+// let time = 1200;
+// const cafe = {
+//     name: "Whitesheep",
+//     seatingCapacity: 100,
+//     hasSpecialOffers: true,
+//     drinks: ["Cappuccino","Latte","Filter coffee","Tea","Hot chocolate"],
+//     breakfastOffer: "Free croissant with coffee",
+//     lunchOffer: "Free drink with surprisingly priced sandwich",
+//     noOffer: "Sorry no offer"
+// };
+// if (time < 1100){
+// offer = cafe.breakfastOffer; console.log(cafe.breakfastOffer);
+// } else if (time < 1500){
+// offer = cafe.lunchOffer; console.log(cafe.lunchOffer);
+// }
+
+// including if/else statements to output correct results in console.log
+
+// let day = "Saturday";
+// const alarm = {
+//     weekendAlarm: "no alarm needed",
+//     weekdayAlarm: "get up at 7am"
+// };
+// if (day == "Saturday" || day == "Sunday") {
+//     console.log(alarm["weekendAlarm"])
+// } else {
+//     console.log(alarm["weekdayAlarm"])
+// };
+
+// pulls if else result into console.log based on "let day = x" result
+
+
+// const person = {
+//     name: "Ben",
+//     age: "32",
+// }
+// console.log (`Hello, my name is ${this.name}`)
+
+const pet = {
+    name: "fred",
+    age: "4",
+    typeOfPet: "cat",
+    colour: "black",
+}
+eatAndDrink
+console.log (`${this.name} is eating or drinking`)
+
+ 
